5c6b19c5f827052ed6b880bfc6d245c31e58ca33
ozanakpnr <ozan@ozbilim.com>
2023-06-09 11:33:35 +0300

    Pastal fare tekeri ile yakınlaşma uzaklaşma özelliği eklendi
    Etiket bilgileri eklendi
    
    Düzeltmeler yapıldı

--- index.js
+++ index.js
@@ -1,39 +1,82 @@
-let Coords=[]; 
-let polyCoords =[];
+//SVG DEĞİLDE PİKSEL OLARAK CANVAS ÇİZİM KODLARI
 //let canvas = document.getElementById("canvasDrawing");
-let svg = document.getElementById("svgDrawing");
-const polygon= document.createElementNS("http://www.w3.org/2000/svg","polygon");
-const fileRatioSelector = document.getElementById("fileRatio");
-let xmax=0 ;
-let ymax=0 ;
-let polyPoints="";
-let ratio=0.1;
-let fileName="";
-
 //var ctx = canvas.getContext("2d");
 //ctx.fillStyle="#fff";
 
+//HTML ELEMENT TANIMLAMALARI
+const polygon= document.createElementNS("http://www.w3.org/2000/svg","polygon");
+var rectangle = document.createElement('div');
 
+//HTML MEVCUT ELEMENTLER
+const svg = document.getElementById("svgDrawing");
+const canvas = document.getElementById("canvasDiv");
+const subDiv = document.getElementById("subDiv");
+const fileRatioSelector = document.getElementById("fileRatio");
+
+//VARIABLES
+
+let xmax=0 ;
+let ymax=0 ;
+let ratio=0.1;
+let polyPoints="";
+let lblPoints="";
+let fileName="";
+let cutInfo=[]; //CUT FILE SPLITTED 
+let Coords=[]; 
+let polyCoords =[];
+
+//DOSYA ORANI DEĞİŞTİ BİLGİSİ
 fileRatioSelector.addEventListener("change",function(){
     ratio = fileRatioSelector.value;
 })
 
+//PASTAL FARE TEKERLEĞİ İLE YAKINLAŞMA
+let scale =1;
+subDiv.style.scale=1;
+canvas.addEventListener("mousewheel",function(event){
+event.preventDefault();
+var delta =Math.max(-1,Math.min(1,event.wheelDelta || -event.detail));
+var mouseX = event.clientX - this.offsetLeft;
+  var mouseY = event.clientY - this.offsetTop;
+    scale+=delta*0.1;
+    scale = Math.max(1,Math.min(4,scale));
+    var offsetX = (mouseX - this.clientWidth / 2) * (scale - 1);
+    var offsetY = (mouseY - this.clientHeight / 2) * (scale - 1);
+    subDiv.style.transformOrigin = mouseX + 'px ' + mouseY + 'px';
+    subDiv.style.transform = 'scale(' + scale + ')';
+
+});
+
 function SelectFile(){
+    //SVG CHILD ELEMENTLERI SIFIRLA
+   let lblDivs=document.querySelectorAll(".lblElement");
+   lblDivs.forEach(function(element){
+    element.remove();
+   })
     svg.innerHTML="";
-   xmax=0 ;
-     ymax=0 ;
-     polyPoints="";
-        Coords=[];
-        polyCoords=[];
-let input = document.createElement("input");
-input.type="file";
-input.onchange=_=>{
-   fileName= input.files[0].name;
+    subDiv.style.scale=1;
+    
+    //DEĞİŞKENLERİ SIFIRLA
+    xmax=0 ;
+    ymax=0 ;
+    polyPoints="";
+    lblPoints="";
+    cutInfo=[];
+    Coords=[];
+    lblCoords=[];
+    polyCoords=[];
+    
+    //SANAL INTPUT OLUŞTUR VE ONCHANGE EVENTİ İLE DOSYAYI YAKALA
+    let input = document.createElement("input");
+    input.type="file";
+    input.onchange=_=>{
+        fileName= input.files[0].name;
+
+    //PASTAL ÇİZİM    
     DrawPolies(input.files[0])
 };
 input.click();
 };
-
 
 function DrawPolies(file){
 
@@ -41,37 +84,13 @@ function DrawPolies(file){
     var selectedFile= reader.readAsText(file,"UTF-8");
     
     reader.onload=function(evt){
-      
        const rawFileContent =evt.target.result;
-const fileContent = rawFileContent.replace(/\n/g,"");
-      // document.getElementById("fileContent").innerHTML= fileContent.split("*");
-       var cutInfo= fileContent.split("*");
-       const startIndex = cutInfo.indexOf("N1");
-       for (i=startIndex; cutInfo[i]!="M0"; i++) {
-        if (cutInfo[i].startsWith("N")) {
-            let k=i+1;
-            for (k; ((!cutInfo[k].startsWith("N"))&&(cutInfo[k]!="M0")); k++) {
-             if (!cutInfo[k].includes("M31")) {
-             
-                    if (cutInfo[k].startsWith("X")||cutInfo[k].startsWith("Q")) {
-                        let x = (cutInfo[k].split("Y")[0]).split("X")[1];
-                        let y = (cutInfo[k].split("Y")[1]);
-                        let actCoord = [x,y];
-                         Coords.push(actCoord);
-                     }
-                
-                
-                 }
-             }
-            
-             polyCoords.push(Coords);
-          Coords=[];
-       
-        }
-        }
+       const fileContent = rawFileContent.replace(/[\r\n]/g,"");
+       cutInfo= fileContent.split("*");
+        processCutFile();
 
         
-      
+      //xmax ve ymax bul
       polyCoords.forEach(element => {
         for (let index = 0; index < element.length; index++) {
             if (ymax<parseInt(element[index][1])) {
@@ -84,8 +103,8 @@ const fileContent = rawFileContent.replace(/\n/g,"");
       });
 
       document.getElementById("markerName").innerHTML="Name: "+fileName;
-document.getElementById("markerLength").innerHTML="Length: "+(xmax*ratio).toFixed(2)+" mm";
-document.getElementById("markerWidth").innerHTML="Width: "+(ymax*ratio).toFixed(2)+" mm";
+      document.getElementById("markerLength").innerHTML="Length: "+(xmax*ratio).toFixed(2)+" mm";
+      document.getElementById("markerWidth").innerHTML="Width: "+(ymax*ratio).toFixed(2)+" mm";
 
 
         //const scaleRatio = canvas.height/ymax;
@@ -128,18 +147,66 @@ polyCoords.forEach(element => {
                 }
     
             })
-         
+            //ETIKET BİLGİSİ VAR İSE KARELERI OLUŞTUR
+            if (lblCoords.length!=0) {
+                const rect = rectangle.cloneNode(true);
+            subDiv.appendChild(rect);
+            rect.classList.add("lblElement");
+            rect.style.zIndex=10;
+            rect.style.top=lblCoords[i][1]*scaleRatio+"px";
+            rect.style.left=lblCoords[i][0]*scaleRatio+"px";
+            }
+            
             //ctx.closePath();        
             //ctx.fill();
          }
-            
- 
+        
        }
       
     reader.onerror=function(evt){
         document.getElementById("fileContent").innerHTML="error reading the file";
     }
   
+}
+
+function processCutFile()
+{
+    const startIndex = cutInfo.indexOf("N1");
+    for (i=startIndex; (cutInfo[i]!="M0")&&(i<cutInfo.length); i++) {
+     if (cutInfo[i].startsWith("N")) {
+         let k=i+1;
+         for (k; ((!cutInfo[k].startsWith("N"))&&(cutInfo[k]!="M0")&&(k+1<cutInfo.length)); k++) {
+             //ETIKET BİLGİSİ YOK
+             if (!cutInfo[k].includes("M31")) {
+          
+                 if (cutInfo[k].startsWith("X")||cutInfo[k].startsWith("Q")) {
+                     if ((!cutInfo[k-1].startsWith("M"))||(!cutInfo[k+1].startsWith("M"))) {
+                         let x = (cutInfo[k].split("Y")[0]).split("X")[1];
+                         let y = (cutInfo[k].split("Y")[1]);
+                         let actCoord = [x,y];
+                          Coords.push(actCoord);
+                     }
+                   
+                  } 
+              } 
+              //ETIKET BILGISI VAR
+              else{
+                 let lblInfo=cutInfo[k+1].split(" ");
+                 if (cutInfo[k].includes("X")&&cutInfo[k].includes("Y")) {
+                    let lblx=(cutInfo[k].split("Y")[0]).split("X")[1];
+                    let lbly=(cutInfo[k].split("Y")[1].split("M")[0]);
+                    let lblCoord = [lblx,lbly];
+                    lblCoords.push(lblCoord);
+                 }
+             
+              }
+          }
+         
+          polyCoords.push(Coords);
+       Coords=[];
+    
+     }
+     }
 }
 
 
